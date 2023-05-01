fe4204475e907d1a407e40e3c82daab8a69f4e88
UrielMartinez21 <uriellmartinezz35@gmail.com>
2023-04-30 20:48:31 -0600

    Diseño pantalla crear cuenta

--- pages/pages/pantallainicio/crearcuenta.js
+++ pages/pages/pantallainicio/crearcuenta.js
@@ -1,26 +1,42 @@
-import React, { useState, useRef, use } from 'react'
-import Link from 'next/link';
+import React, { useState, useRef } from 'react'
 import Head from 'next/head';
 import { Toast } from 'primereact/toast';
 import { Button } from 'primereact/button';
 import { Password } from 'primereact/password';
+import { useRouter } from 'next/router';
 import { InputText } from "primereact/inputtext";
+import { Messages } from 'primereact/messages';
 import AppConfig from '@/layout/AppConfig';
 
 const CrearCuenta = () => {
+  //--> Variaable de redireccinamiento
+  const router = useRouter();
+
   //-----------------------| Lista de variables |-----------------------
   const toast = useRef(null);
+  const msgs = useRef(null);
   const [email, setEmail] = useState('')
   const [nombre, setNombre] = useState('')
   const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
   //--> Variables para validar envio
   const [estiloEmail, setEstiloEmail] = useState('')
   const [estiloNombre, setEstiloNombre] = useState('')
   const [estiloPassword, setEstiloPassword] = useState('')
-  const [mensajeAdvertencia, setMensajeAdvertencia] = useState('')
+  const [estiloConfirmPass, setEstiloConfirmPass] = useState('')
+  //--> Mensajes de advertencia
+  // const [mensajeAdvertencia, setMensajeAdvertencia] = useState('')
 
   //-----------------------| Expresion regular |-----------------------
   const validarEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/
+
+  const mostrarMensaje = (mensaje) => {
+    msgs.current.show(
+      { severity: 'error', detail: `${mensaje}`, sticky: true, closable: false },
+    );
+  };
+
+  const limpiarMensaje = () => { msgs.current.clear() }
 
   const crearUsuario = (e) => {
     e.preventDefault()
@@ -28,32 +44,76 @@ const CrearCuenta = () => {
       if (!email) setEstiloEmail('p-invalid')
       if (!nombre) setEstiloNombre('p-invalid')
       if (!password) setEstiloPassword('p-invalid')
-      setMensajeAdvertencia("Todos los campos son validos")
-      setTimeout(() => { setMensajeAdvertencia('') }, 3000)
+      if (!confirmPassword) setEstiloConfirmPass('p-invalid')
+      // setMensajeAdvertencia("Todos los campos son validos")
+      // setTimeout(() => { setMensajeAdvertencia('') }, 3000)
+      mostrarMensaje("Todos los campos son validos")
+      setTimeout(() => { limpiarMensaje() }, 3000)
       return
     } else {
       setEstiloEmail('')
       setEstiloNombre('')
       setEstiloPassword('')
+      setEstiloConfirmPass('')
     }
+    //--> Validar email
     if (!validarEmail.test(email)) {
       setEstiloEmail('p-invalid')
-      setMensajeAdvertencia("El email no es valido")
-      setTimeout(() => { setMensajeAdvertencia('') }, 3000)
+      // setMensajeAdvertencia("El email no es valido")
+      // setTimeout(() => { setMensajeAdvertencia('') }, 3000)
+      mostrarMensaje("El email no es valido")
+      setTimeout(() => { limpiarMensaje() }, 3000)
       return
     } else { setEstiloEmail('') }
+    //--> Validar password
     if (password.length < 6) {
       setEstiloPassword('p-invalid')
-      setMensajeAdvertencia("La contraseña es muy corta")
-      setTimeout(() => { setMensajeAdvertencia('') }, 3000)
+      // setMensajeAdvertencia("La contraseña es muy corta")
+      // setTimeout(() => { setMensajeAdvertencia('') }, 3000)
+      mostrarMensaje("La contraseña es muy corta")
+      setTimeout(() => { limpiarMensaje() }, 3000)
       return
     } else { setEstiloPassword('') }
-
+    //--> Validar confirmar password
+    if (confirmPassword.length < 6) {
+      setEstiloConfirmPass('p-invalid')
+      // setMensajeAdvertencia("La contraseña es muy corta")
+      // setTimeout(() => { setMensajeAdvertencia('') }, 3000)
+      mostrarMensaje("La contraseña es muy corta")
+      setTimeout(() => { limpiarMensaje() }, 3000)
+      return
+    } else { setEstiloConfirmPass('') }
+    //--> Comprobar contraseñas iguales
+    if (password !== confirmPassword) {
+      setEstiloPassword('p-invalid')
+      setEstiloConfirmPass('p-invalid')
+      // setMensajeAdvertencia("Son diferentes las contraseñas")
+      // setTimeout(() => { setMensajeAdvertencia('') }, 3000)
+      mostrarMensaje("Son diferentes las contraseñas")
+      setTimeout(() => { limpiarMensaje() }, 3000)
+      return
+    } else {
+      setEstiloPassword('')
+      setEstiloConfirmPass('')
+    }
+    //--> Limpiar campos
     setEmail('')
     setNombre('')
     setPassword('')
-
+    setEstiloConfirmPass('')
+    //--> Notificar estatus
     toast.current.show({ severity: 'success', summary: 'Usuario creado!', detail: 'Revisa tu correo', life: 3000 });
+  }
+
+  const cancelarCreacion = () => {
+    //--> Limpiar campos antes de salio
+    setNombre('')
+    setEmail('')
+    setPassword('')
+    setConfirmPassword('')
+
+    //--> Redireccionar
+    router.push('/')
   }
 
   return (
@@ -77,38 +137,66 @@ const CrearCuenta = () => {
         <Toast ref={toast} />
         <div className="col-12">
           <div className="card">
-            <h5 className='card text-center'>Llena el formulario para crear tu cuenta</h5>
+            <h5 className='card text-center'>Crear cuenta</h5>
 
-            <form className="flex flex-wrap gap-3 p-fluid" onSubmit={crearUsuario}>
-              <div className="flex-initial">
-                <label htmlFor="nombreCompleto" className="font-bold block mb-2">Ingresa tu nombre Completo</label>
-                <span className="p-input-icon-left">
-                  <i className="pi pi-user" />
-                  <InputText id="nombreCompleto" placeholder="Nombre completo" className={estiloNombre}
+            <div className="flex flex-column card-container card" onSubmit={crearUsuario}>
+              <div className='flex align-items-center justify-content-center'>
+                <div>
+                  <label htmlFor="nombreCompleto" className="block text-900 text-xl font-medium mb-2">Tu nombre</label>
+                  <InputText id="nombreCompleto" placeholder="Nombre completo"
+                    // className='flex align-items-center justify-content-center w-full md:w-30rem mb-5' style={{ padding: '1rem' }}
+                    className={`${estiloNombre} flex align-items-center justify-content-center w-full md:w-30rem mb-5`}
+                    style={{ padding: '1rem' }}
                     value={nombre} onChange={(e) => { setNombre(e.target.value) }} />
-                </span>
+                </div>
               </div>
-              <div>
-                <label htmlFor="correo" className="font-bold block mb-2">Ingresa tu correo</label>
-                <span className="p-input-icon-left">
-                  <i className="pi pi-envelope" />
-                  <InputText id="correo" placeholder="Correo" className={estiloEmail}
+              <div className='flex align-items-center justify-content-center'>
+                <div>
+                  <label htmlFor="correo" className="block text-900 text-xl font-medium mb-2">Correo electrónico</label>
+                  <InputText
+                    id="correo" placeholder="Correo"
+                    className={`${estiloEmail} flex align-items-center justify-content-center w-full md:w-30rem mb-5`}
+                    style={{ padding: '1rem' }}
                     value={email} onChange={(e) => { setEmail(e.target.value) }} />
-                </span>
+                </div>
               </div>
-              <div>
-                <label htmlFor="password" className="font-bold block mb-2">Ingresa tu contraseña</label>
-                <Password className={estiloPassword} value={password} onChange={(e) => setPassword(e.target.value)} toggleMask />
+              <div className='flex align-items-center justify-content-center'>
+                <div>
+                  <label htmlFor="password" className="block text-900 text-xl font-medium mb-2">Contraseña</label>
+                  <Password
+                    className={`${estiloPassword} flex align-items-center justify-content-center w-full md:w-30rem mb-5`}
+                    style={{ padding: '1rem' }}
+                    value={password} onChange={(e) => setPassword(e.target.value)} toggleMask />
+                </div>
+              </div>
+              <div className='flex align-items-center justify-content-center'>
+                <div>
+                  <label htmlFor="password" className="block text-900 text-xl font-medium mb-2">Confirmar contraseña</label>
+                  <Password
+                    className={`${estiloConfirmPass} flex align-items-center justify-content-center w-full md:w-30rem mb-5`}
+                    style={{ padding: '1rem' }}
+                    value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} toggleMask />
+                </div>
               </div>
 
-              {mensajeAdvertencia && (<p className='font-bold text-center bg-red-600 text-white mt-4 py-2'>{mensajeAdvertencia}</p>)}
+              {/* {mensajeAdvertencia && (<p className='font-bold text-center bg-red-600 text-white mt-4 py-2'>{mensajeAdvertencia}</p>)} */}
+              <div className='flex justify-content-center flex-wrap card-container'>
+                <div className='flex align-items-center justify-content-center'>
+                  <div className='W-Full'>
+                    <Messages ref={msgs} />
+                  </div>
+                </div>
+              </div>
+            </div>
 
-              <Button label="Crear cuenta" className="w-full p-3 text-xl" type='submit' />
-            </form>
+            <div className='flex justify-content-around flex-wrap card-container'>
+              <Button
+                label="Cancelar" onClick={cancelarCreacion} severity="danger"
+                className='flex align-items-center justify-content-center' />
+              <Button label="Aceptar" onClick={crearUsuario} severity="success"
+                className='flex align-items-center justify-content-center' />
+            </div>
 
-            <Link href="/" className="font-medium no-underline ml-2 text-right cursor-pointer" style={{ color: 'var(--primary-color)' }}>
-              Regresar
-            </Link>
 
           </div>
         </div>
