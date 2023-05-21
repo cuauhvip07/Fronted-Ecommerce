4ef1adbf8f14a31eb1509d6b0b63a9966b6c1250
UrielMartinez21 <uriellmartinezz35@gmail.com>
2023-05-21 15:11:54 -0600

    Coreccion pantalla recuperar password

--- components/mensajesNotificaciones/notificaciones.js
+++ components/mensajesNotificaciones/notificaciones.js
@@ -2,4 +2,6 @@
 export const usuarioCreado = { titulo: "¡Usuario creado!", contenido: "Revisa tu correo." }
 export const cuentaConfirmada = { titulo: "¡Cuenta confirmada!", contenido: "Ya puedes iniciar sesión." }
 export const envioToken = { titulo: "¡Token reenviado!", contenido: "Vuelve a revisar tu correo." }
-export const restablecerPass = { titulo: "¡Revisa tu correo!", contenido: "Hemos enviado un codigo para restablecer tu contraseña." }
+export const restablecerPass = {
+  titulo: "¡Revisa tu correo!", contenido: "Hemos enviado un codigo para restablecer tu contraseña."
+}
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,15 @@
+import React, { useContext, useState, useRef } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import Head from 'next/head';
-import React, { useContext, useState } from 'react';
 import AppConfig from '@/layout/AppConfig';
 // import { Checkbox } from 'primereact/checkbox';
+import { Toast } from 'primereact/toast';
 import { Button } from 'primereact/button';
 import { Password } from 'primereact/password';
-import { LayoutContext } from '@/layout/context/layoutcontext';
 import { InputText } from 'primereact/inputtext';
 import { classNames } from 'primereact/utils';
-
-
+import { LayoutContext } from '@/layout/context/layoutcontext';
 
 import * as components from './components';
 import Image from 'next/image';
@@ -18,23 +17,28 @@ import myImage from '../imagenes/login/loto.jpg';
 import myImage1 from '../imagenes/login/flower1.jpeg';
 import loto from '../imagenes/login/principal1.png';
 import styles from '../styles/styles.module.css';
+import { restablecerPass } from '@/components/mensajesNotificaciones/notificaciones';
 
 
 
 export default function Home() {
-  //--> Variables
-  const [signIn, toggle]= useState(true);
-
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('');
-  // const [checked, setChecked] = useState(false);
-  const [estiloEmail, setEstiloEmail] = useState('')
-  const [estiloPassword, setEstiloPassword] = useState('')
-  const [mensajeAdvertencia, setMensajeAdvertencia] = useState('')
-
-
   //--> Uso de contexto
   const { layoutConfig } = useContext(LayoutContext);
+
+  //--> Mensajes y notificaciones
+  const toast = useRef(null);
+
+  //----------------| Lista de variables |----------------
+  const [signIn, toggle] = useState(true);
+  // --> Campos de entrada
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('');
+  const [emailrecuperar, setEmailrecuperar] = useState('')
+  //--> Estilos
+  const [estiloEmail, setEstiloEmail] = useState('')
+  const [estiloPassword, setEstiloPassword] = useState('')
+  const [estiloEmailRec, setEstiloEmailRec] = useState('')
+  const [mensajeAdvertencia, setMensajeAdvertencia] = useState('')
 
   const router = useRouter();
   const containerClassName = classNames('surface-ground flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden', { 'p-input-filled': layoutConfig.inputStyle === 'filled' });
@@ -69,6 +73,30 @@ export default function Home() {
     router.push('/pages/dashboard')
   }
 
+
+  const validarEnvio2 = () => {
+    if ([emailrecuperar].includes('')) {
+      setEstiloEmailRec('p-invalid')
+      setMensajeAdvertencia("Todos los campos son obligatorios")
+      setTimeout(() => { setMensajeAdvertencia('') }, 3000)
+      return
+    } else { setEstiloEmailRec('') }
+    if (!validarEmail.test(emailrecuperar)) {
+      setEstiloEmailRec('p-invalid')
+      setMensajeAdvertencia("El email no es valido")
+      setTimeout(() => { setMensajeAdvertencia('') }, 3000)
+      return
+    } else { setEstiloEmailRec('') }
+    // --> Limpiar variables
+    setMensajeAdvertencia('')
+    setEmailrecuperar('')
+    // --> Informar estatus 
+    toast.current.show({
+      severity: 'success', summary: `${restablecerPass.titulo}`, detail: `${restablecerPass.contenido}`, life: 3000
+    });
+  }
+
+
   //---------------------------| Valor que regresara |---------------------------
   return (
     <>
@@ -87,97 +115,98 @@ export default function Home() {
         <link rel="icon" href={`/favicon.ico`} type="image/x-icon"></link>
       </Head>
 
-      
+
       <components.Container className={`card m-auto mt-8 ${styles.card}`} >
-      <components.SignUpContainer className={`card ${styles.card}`} signinIn={signIn}>
-      <components.Form  >
-        
-     
-        <h1 className={`font-bold text-center`}>Recuperar contraseña</h1>
-        <components.Parrafo>Ingresa el correo asociado a tu cuenta</components.Parrafo>
-        <label htmlFor="email1" className="block text-900 ">Email</label>
-                <InputText
-                  inputid="email1" value={email} onChange={(e) => setEmail(e.target.value)}
-                  type="text" placeholder="Email address" className={`block text-900  mb-2 w-full p-3  ${estiloEmail}`} 
-                   />
-
-        <Button label="Enviar" className="w-full p-3 text-xl" title="enviar" onClick={validarEnvio} />
-        {mensajeAdvertencia && (<p className='font-bold text-center bg-red-600 text-white mt-4 py-2'>{mensajeAdvertencia}</p>)}
-        <components.Anchor onClick={()=> toggle(true)}  >Iniciar Sesión</components.Anchor>
-        
-          
-        </components.Form  >
-      </components.SignUpContainer>
+        <Toast ref={toast} />
+        <components.SignUpContainer className={`card ${styles.card}`} signinIn={signIn}>
+          <components.Form  >
 
 
-      <components.SignInContainer   className={`card ${styles.card}`} signinIn={signIn}>
-        
-        <components.Form >
-        <Image src={loto} className={styles['logo']}alt="Mi imagen"  priority={true} />
-        <h1 className={`font-bold text-center`}>Iniciar Sesión</h1>
-        
-        <label htmlFor="email1" className="block text-900 ">Correo</label>
-                <InputText
-                  inputid="email1" value={email} onChange={(e) => setEmail(e.target.value)}
-                  type="text" placeholder="Correo electrónico" className={`block text-900  mb-2 w-full p-3  ${estiloEmail}`} 
-                   />
+            <h1 className={`font-bold text-center`}>Recuperar contraseña</h1>
+            <components.Parrafo>Ingresa el correo asociado a tu cuenta</components.Parrafo>
+            <label htmlFor="email1" className="block text-900 ">Email</label>
+            <InputText
+              inputid="email1" value={emailrecuperar} onChange={(e) => setEmailrecuperar(e.target.value)}
+              type="text" placeholder="Email address" className={`block text-900  mb-2 w-full p-3  ${estiloEmailRec}`}
+            />
 
-                <label htmlFor="password1" className="block text-900 ">Contraseña</label>
-                <Password
-                  inputid="password1" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Contraseña"
-                  feedback={false} className="w-full " inputClassName={`w-full p-3 md:w-30rem  ${estiloPassword}`} />
-
-                <components.Parrafo onClick={()=> toggle(false)} className="font-medium no-underline ml-2 text-right cursor-pointer" style={{ color: 'var(--primary-color)' }}>¿Olvidaste tu contraseña?</components.Parrafo>
-                <Button label="Iniciar Sesion" className="w-full p-3 mb-3 text-xl" onClick={validarEnvio} />
-
-                {mensajeAdvertencia && (<p className='font-bold text-center bg-red-600 text-white mt-4 py-2'>{mensajeAdvertencia}</p>)}
+            <Button label="Enviar" className="w-full p-3 text-xl" title="enviar" onClick={validarEnvio2} />
+            {mensajeAdvertencia && (<p className='font-bold text-center bg-red-600 text-white mt-4 py-2'>{mensajeAdvertencia}</p>)}
+            <components.Anchor onClick={() => toggle(true)}  >Iniciar Sesión</components.Anchor>
 
 
+          </components.Form  >
+        </components.SignUpContainer>
 
-          
-          <components.Parrafo>¿Eres Nuevo?</components.Parrafo>
-          <div className="flex align-items-center">
-                    <Link
-                      href="/pages/pantallainicio/crearcuenta"
-                      className="font-medium no-underline ml-2 text-right cursor-pointer" style={{ color: 'var(--primary-color)' }}
-                    >Crear cuenta</Link>
-          </div>
-                
-        </components.Form>
-      </components.SignInContainer>
+
+        <components.SignInContainer className={`card ${styles.card}`} signinIn={signIn}>
+
+          <components.Form >
+            <Image src={loto} className={styles['logo']} alt="Mi imagen" priority={true} />
+            <h1 className={`font-bold text-center`}>Iniciar Sesión</h1>
+
+            <label htmlFor="email1" className="block text-900 ">Correo</label>
+            <InputText
+              inputid="email1" value={email} onChange={(e) => setEmail(e.target.value)}
+              type="text" placeholder="Correo electrónico" className={`block text-900  mb-2 w-full p-3  ${estiloEmail}`}
+            />
+
+            <label htmlFor="password1" className="block text-900 ">Contraseña</label>
+            <Password
+              inputid="password1" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Contraseña"
+              feedback={false} className="w-full " inputClassName={`w-full p-3 md:w-30rem  ${estiloPassword}`} />
+
+            <components.Parrafo onClick={() => toggle(false)} className="font-medium no-underline ml-2 text-right cursor-pointer" style={{ color: 'var(--primary-color)' }}>¿Olvidaste tu contraseña?</components.Parrafo>
+            <Button label="Iniciar Sesion" className="w-full p-3 mb-3 text-xl" onClick={validarEnvio} />
+
+            {mensajeAdvertencia && (<p className='font-bold text-center bg-red-600 text-white mt-4 py-2'>{mensajeAdvertencia}</p>)}
 
 
 
 
-      <components.OverlayContainer signinIn={signIn}>
-        <components.Overlay signinIn={signIn}>
+            <components.Parrafo>¿Eres Nuevo?</components.Parrafo>
+            <div className="flex align-items-center">
+              <Link
+                href="/pages/pantallainicio/crearcuenta"
+                className="font-medium no-underline ml-2 text-right cursor-pointer" style={{ color: 'var(--primary-color)' }}
+              >Crear cuenta</Link>
+            </div>
 
-          <components.leftOverLayPanel signinIn={signIn}>
-          <components.GhostButton onClick={()=> toggle(true)}>Iniciar Sesión</components.GhostButton>
-          </components.leftOverLayPanel>
+          </components.Form>
+        </components.SignInContainer>
 
 
-          <components.RightOverLayPanel  signinIn={signIn}>
-          <components.Title>Bienvenido de nuevo!</components.Title>
-          <components.Title2>Jardín del Edén</components.Title2>
-          
-          </components.RightOverLayPanel >
-          <Image src={myImage1} className={styles['my-image']} alt="Mi imagen"  priority={true} />
-          <Image src={myImage} className={styles['my-image']} alt="Mi imagen"  priority={true} />
-        </components.Overlay>
-      </components.OverlayContainer>
-      
-      
+
+
+        <components.OverlayContainer signinIn={signIn}>
+          <components.Overlay signinIn={signIn}>
+
+            <components.leftOverLayPanel signinIn={signIn}>
+              <components.GhostButton onClick={() => toggle(true)}>Iniciar Sesión</components.GhostButton>
+            </components.leftOverLayPanel>
+
+
+            <components.RightOverLayPanel signinIn={signIn}>
+              <components.Title>Bienvenido de nuevo!</components.Title>
+              <components.Title2>Jardín del Edén</components.Title2>
+
+            </components.RightOverLayPanel >
+            <Image src={myImage1} className={styles['my-image']} alt="Mi imagen" priority={true} />
+            <Image src={myImage} className={styles['my-image']} alt="Mi imagen" priority={true} />
+          </components.Overlay>
+        </components.OverlayContainer>
+
+
       </components.Container>
       <AppConfig />
-    
-     
 
-     
-      
-      
+
+
+
+
+
     </>
-    
+
   )
 }
 
