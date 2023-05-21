4508014f5d7bbc66619caa494e700d71ae55abea
UrielMartinez21 <uriellmartinezz35@gmail.com>
2023-05-21 10:00:54 -0600

    Pantalla carrito de compras y dashboard

--- pages/pages/dashboard.js
+++ pages/pages/dashboard.js
@@ -1,6 +1,45 @@
+import React, { useState } from "react";
 import Layout from "@/layout/layout"
+import { Carousel } from 'primereact/carousel';
+
 
 const Dashboard = () => {
+  const [products, setProducts] = useState([]);
+  const responsiveOptions = [
+    { breakpoint: '1199px', numVisible: 1, numScroll: 1 },
+    { breakpoint: '991px', numVisible: 2, numScroll: 1 },
+    { breakpoint: '767px', numVisible: 1, numScroll: 1 }
+  ];
+
+  const temporadas = [
+    { temporada: "Invierno", duracion: "10 dias" },
+    { temporada: "Primavera", duracion: "12 dias" },
+    { temporada: "Otoño", duracion: "1 dias" },
+    { temporada: "Año nuevo", duracion: "3 dias" },
+    { temporada: "San valentin", duracion: "5 dias" },
+  ]
+
+  const plantillaTemporada = (temporada) => {
+    return (
+      <div className="border-1 surface-border border-round m-2 text-center py-5 px-3">
+        <div className="mb-3">
+          {/* <img src={`https://primefaces.org/cdn/primereact/images/product/${temporada.image}`}
+            alt={temporada.name} className="w-6 shadow-2" /> */}
+        </div>
+        <div>
+          <h4 className="mb-1">{temporada.temporada}</h4>
+          <h6 className="mt-0 mb-3">{temporada.duracion}</h6>
+          {/* <Tag value={temporada.inventoryStatus} severity={getSeverity(temporada)}></Tag> */}
+          {/* <div className="mt-5 flex flex-wrap gap-2 justify-content-center">
+            <Button icon="pi pi-search" className="p-button p-button-rounded" />
+            <Button icon="pi pi-star-fill" className="p-button-success p-button-rounded" />
+          </div> */}
+        </div>
+      </div>
+    );
+  };
+
+
   return (
     <Layout
       title="Inicio"
@@ -10,6 +49,9 @@ const Dashboard = () => {
         <div className="col-12">
           <div className="card">
             <h5>Pagina principal</h5>
+            <Carousel value={temporadas} numVisible={3} numScroll={3}
+              responsiveOptions={responsiveOptions} className="custom-carousel" circular
+              autoplayInterval={3000} itemTemplate={plantillaTemporada} />
           </div>
         </div>
       </div>
--- pages/pages/usuario/carrito.js
+++ pages/pages/usuario/carrito.js
@@ -1,6 +1,113 @@
 import Layout from "@/layout/layout"
+import React, { useEffect, useState } from "react";
+import { DataView } from 'primereact/dataview';
+import { Button } from 'primereact/button';
+import { Image } from 'primereact/image';
+import { Tag } from 'primereact/tag';
+
 
 const CarritoCompras = () => {
+
+  const [flores, setFlores] = useState([])
+
+  useEffect(() => {
+    const datosFlores = [
+      { nombre: "Rosa", precio: 5.90, cantidad: "1", estatus: "Disponible" },
+      { nombre: "Tulipan", precio: 6.20, cantidad: "2", estatus: "Agotado" },
+      { nombre: "Girasol", precio: 3.50, cantidad: "3", estatus: "Pocos" },
+      { nombre: "Setosa", precio: 25.23, cantidad: "2", estatus: "Pocos" },
+      { nombre: "Gardenia", precio: 78.60, cantidad: "1", estatus: "Disponible" },
+      { nombre: "Versicolor", precio: 84.69, cantidad: "4", estatus: "Agotado" },
+    ]
+    setFlores(datosFlores)
+  }, [])
+
+  const getSeverity = (flor) => {
+    switch (flor.estatus) {
+      case 'Disponible':
+        return 'success';
+
+      case 'Pocos':
+        return 'warning';
+
+      case 'Agotado':
+        return 'danger';
+
+      default:
+        return null;
+    }
+  };
+
+  const getTotalPrice = () => {
+    let total = 0;
+    flores.forEach((flor) => {
+      total += parseFloat(flor.precio) * parseInt(flor.cantidad);
+    });
+    return total.toFixed(2);
+  };
+
+  const itemTemplate = (flor) => {
+
+    const incrementar = (flor) => {
+      const updatedFlores = flores.map((f) => {
+        if (f.nombre === flor.nombre) {
+          return { ...f, cantidad: (parseInt(f.cantidad) + 1).toString() };
+        }
+        return f;
+      });
+      setFlores(updatedFlores);
+    };
+
+    const disminuir = (flor) => {
+      const updatedFlores = flores.map((f) => {
+        if (f.nombre === flor.nombre) {
+          const cantidad = parseInt(f.cantidad) - 1;
+          const updatedCantidad = cantidad >= 0 ? cantidad.toString() : f.cantidad;
+          if (updatedCantidad === "0") {
+            return null; // Return null to remove the card from the list
+          }
+          return { ...f, cantidad: updatedCantidad };
+        }
+        return f;
+      });
+      const filteredFlores = updatedFlores.filter((f) => f !== null);
+      setFlores(filteredFlores);
+    };
+
+    const eliminar = (flor) => {
+      const filteredFlores = flores.filter((f) => f.nombre !== flor.nombre);
+      setFlores(filteredFlores);
+    };
+
+    return (
+      <div className="col-12">
+        <div className="flex flex-column xl:flex-row xl:align-items-start p-4 gap-4">
+          <div className="card flex justify-content-center">
+            <Image src="https://primefaces.org/cdn/primereact/images/galleria/galleria7.jpg" alt="Image" width="100" />
+          </div>
+          <div className="flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4">
+            <div className="flex flex-column align-items-center sm:align-items-start gap-3">
+              <div className="text-2xl font-bold text-900">{flor.nombre}</div>
+              {/*<Rating value={product.rating} readOnly cancel={false}></Rating>*/}
+              <div className="flex align-items-center">
+                <Tag value={flor.estatus} severity={getSeverity(flor)}></Tag>
+              </div>
+              <div className="flex align-items-center gap-3">
+                <Button onClick={() => disminuir(flor)} icon="pi pi-minus" className="p-button-square" severity="warning"></Button>
+                <span className="text-5xl font-semibold">{flor.cantidad}</span>
+                <Button onClick={() => incrementar(flor)} icon="pi pi-plus" className="p-button-square" severity="success"></Button>
+              </div>
+            </div>
+            <div className="flex sm:flex-column align-items-center sm:align-items-end gap-3 sm:gap-8">
+              <span className="text-5xl font-semibold">${flor.precio}</span>
+              <Button onClick={() => eliminar(flor)} icon="pi pi-times" className="p-button-rounded" severity="danger"></Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <Layout
       title="Carrito"
@@ -8,8 +115,21 @@ const CarritoCompras = () => {
     >
       <div className="grid">
         <div className="col-12">
+          <h1>Carrito de compras</h1>
+        </div>
+        <div className="col-8">
           <div className="card">
-            <h5>Carrito de compras</h5>
+            <DataView value={flores} itemTemplate={itemTemplate} />
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="card">
+            <div className="flex align-items-center">
+              <h2>Total: ${getTotalPrice()}</h2>
+            </div>
+            <div className="flex align-items-left">
+              <Button label="Pagar" severity="success" />
+            </div>
           </div>
         </div>
       </div>
