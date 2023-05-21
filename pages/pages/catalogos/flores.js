d62ede96551599530858d17ce8a90e724e44c67c
UrielMartinez21 <uriellmartinezz35@gmail.com>
2023-05-20 19:56:17 -0600

    Mostrar detalles de producto

--- pages/pages/catalogos/flores.js
+++ pages/pages/catalogos/flores.js
@@ -1,67 +1,52 @@
 import React, { useEffect, useState } from "react";
 import Layout from "@/layout/layout"
-import { Button } from 'primereact/button';
-import { DataView, DataViewLayoutOptions } from 'primereact/dataview';
-import { Paginator } from 'primereact/paginator';
-// import { Rating } from 'primereact/rating';
 import { Tag } from 'primereact/tag';
-import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
 import { Dialog } from 'primereact/dialog';
-
-
+import { InputText } from 'primereact/inputtext';
+import { DataView, DataViewLayoutOptions } from 'primereact/dataview';
+// import { Rating } from 'primereact/rating';
 
 const CatalogoFlores = () => {
-  //--> Variables
+  //----------------| Lista de variables |----------------
   const [flores, setFlores] = useState([])
   const [layout, setLayout] = useState('grid');
+  //-->Detalles de flor
+  const [detallesFlor, setDetallesFlor] = useState({})
+  const [mostrarDialog, setMostrarDialog] = useState(false)
 
   //--> Ejecucion en segundo plano
   useEffect(() => {
     const datosFlores = [
-      { nombre: "Rosa", precio: 100000, categoria: "primavera", estatus: "disponible" },
-      { nombre: "Tulipan", precio: 6.20, categoria: "otoño", estatus: "agotado" },
-      { nombre: "Girasol", precio: 3.50, categoria: "invierno", estatus: "pocos" },
-      { nombre: "Setosa", precio: 25.23, categoria: "primavera", estatus: "pocos" },
-      { nombre: "Gardenia", precio: 78.60, categoria: "verano", estatus: "disponible" },
-      { nombre: "Versicolor", precio: 84.69, categoria: "verano", estatus: "agotado" },
+      {
+        nombre: "Rosa", precio: 5.90, categoria: "primavera", estatus: "disponible",
+        imagen: "https://png.pngtree.com/png-vector/20210710/ourmid/pngtree-close-up-of-rose-simulation-growth-png-image_3580749.jpg", descripcion: "Descripcion de rosa"
+      },
+      {
+        nombre: "Tulipan", precio: 6.20, categoria: "otoño", estatus: "agotado",
+        imagen: "https://w7.pngwing.com/pngs/666/928/png-transparent-tulip-free-content-flower-georgia-bulldogs-leaf-heart-computer-wallpaper.png",
+        descripcion: "Descripcion de tulipan"
+      },
+      {
+        nombre: "Girasol", precio: 3.50, categoria: "invierno", estatus: "pocos",
+        imagen: "https://media.admagazine.com/photos/61eb22cb9b19d943aa117b30/master/w_1600%2Cc_limit/Girasol.jpg",
+        descripcion: "Descripcion de girasol"
+      },
+      {
+        nombre: "Setosa", precio: 25.23, categoria: "primavera", estatus: "pocos",
+        imagen: "https://img1.freepng.es/20180314/vbq/kisspng-bird-echeveria-agavoides-echeveria-setosa-graptope-lotus-design-material-5aa973bd8fb253.3676960215210546535886.jpg", descripcion: "Descripcion de setosa"
+      },
+      {
+        nombre: "Gardenia", precio: 78.60, categoria: "verano", estatus: "disponible",
+        imagen: "https://us.123rf.com/450wm/rprongjai/rprongjai1910/rprongjai191000001/131915934-flores-de-gardenia-sobre-fondo-blanco.jpg?ver=6", descripcion: "Descripcion de gardenia"
+      },
+      {
+        nombre: "Versicolor", precio: 84.69, categoria: "verano", estatus: "agotado",
+        imagen: "https://img2.freepng.es/20180409/wce/kisspng-cut-flowers-iris-versicolor-iris-5acb7fdb4bc3d7.6318569115232859793103.jpg", descripcion: "Descripcion de versicolor"
+      },
     ]
     setFlores(datosFlores)
   }, [])
-
-
-  const [displayBasic, setDisplayBasic] = useState(false);
-
-  const onClick = () => {
-    setDisplayBasic(true);
-  };
-
-  const onHide = () => {
-    setDisplayBasic(false);
-  };
-
-  const [filteredFlores, setFilteredFlores] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
-  useEffect(() => {
-    const filterFlores = flores.filter(flor => flor.nombre.toLowerCase().includes(searchTerm.toLowerCase()));
-    setFilteredFlores(filterFlores);
-  }, [flores, searchTerm]);
-
-  const renderFooter = () => {
-    return (
-      <div className="dialog-footer">
-        <Button label="Cerrar" onClick={ onHide} />
-      </div>
-    );
-  };
-  const onSearch = (e) => {
-    setSearchTerm(e.target.value);
-  };
-
-  
-
-  
-
-
 
   //--> Indicar estado de la flor
   const getSeverity = (flor) => {
@@ -83,17 +68,13 @@ const CatalogoFlores = () => {
   //--> Modo de vista: lista
   const listItem = (flor) => {
     return (
-
-      
       <div className="col-12">
+        <div className="flex flex-column xl:flex-row xl:align-items-start p-4 gap-4">
 
-        <div className="flex flex-column xl:flex-row xl:align-items-start p-4 gap-4 ">
-          <img className="w-9 sm:w-16rem xl:w-10rem shadow-2 block xl:block mx-auto border-round shadow-5 border-round" src={`https://media.admagazine.com/photos/61eb22cb9b19d943aa117b30/master/w_1600%2Cc_limit/Girasol.jpg`} alt="girasol" />
-          
+          <img className="w-9 sm:w-16rem xl:w-10rem shadow-2 block xl:block mx-auto border-round" src={`${flor.imagen}`} alt={`${flor.nombre}`} style={{ width: '200px', height: '200px' }} />
           <div className="flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4">
-            <div className="flex flex-column align-items-center sm:align-items-start ">
+            <div className="flex flex-column align-items-center sm:align-items-start gap-3">
               <div className="text-2xl font-bold text-900">{flor.nombre}</div>
-              <span className="text-2xl font-semibold sh">${flor.precio}</span>
               {/* <Rating value={flor.rating} readOnly cancel={false}></Rating> */}
               <div className="flex align-items-center gap-3">
                 <Tag value={flor.estatus} severity={getSeverity(flor)}></Tag>
@@ -102,72 +83,53 @@ const CatalogoFlores = () => {
                   <span className="font-semibold">{flor.categoria}</span>
                 </span>
               </div>
-             
-                
+              <span className="text-2xl font-semibold mt-8">${flor.precio}</span>
             </div>
-            
+
             <div className="flex sm:flex-column align-items-center sm:align-items-end gap-3 sm:gap-2">
-              {/* <span className="text-2xl font-semibold">${flor.precio}</span> */}
-              
-              <div>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                 Ut enim ad minim veniam, quis nostrud exercitation </p> 
-              </div> 
-              <Button   label="Favoritos" icon="pi pi-heart" rounded severity="help" aria-label="Favorite" className="p-button-rounded"  />
-              <Button label="Agregar" icon="pi pi-shopping-cart" className="p-button-rounded" disabled={flor.estatus === 'agotado'}></Button>
-              <Button label="Detalles" icon="pi pi-external-link"className="p-button-rounded" onClick={() => onClick('displayBasic')} />
-                
-            <Dialog header="Rosa" visible={displayBasic} style={{ width: '20vw' }} footer={renderFooter('displayBasic')} onHide={() => onHide('displayBasic')}>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation </p>    
-            </Dialog>
-              
+              <Button label="Favoritos" icon="pi pi-heart" rounded severity="help"
+                aria-label="Favorite" className="p-button-rounded" />
+              <Button label="Agregar" icon="pi pi-shopping-cart" className="p-button-rounded"
+                disabled={flor.estatus === 'agotado'} />
+              <Button label="Detalles" icon="pi pi-external-link" className="p-button-rounded"
+                onClick={() => dialogoFlor(flor)} />
             </div>
+
           </div>
         </div>
       </div>
     );
   };
-  
 
   //--> Modo de vista: grid
   const gridItem = (flor) => {
     return (
-      <div className="col-12 sm:col-6 lg:col-12 xl:col-4 p-2 border-round">
-        <div className="p-4 border-1 surface-border surface-card border-round shadow-2 ">
-          <div className="flex flex-wrap align-items-center justify-content-between gap-2 border-round ">
-            <div className="flex align-items-center gap-2 border-round">
+      <div className="col-12 sm:col-6 lg:col-12 xl:col-4 p-2">
+        <div className="p-4 border-1 surface-border surface-card border-round">
+
+          <div className="flex flex-wrap align-items-center justify-content-between gap-2">
+            <div className="flex align-items-center gap-2">
               <i className="pi pi-tag"></i>
               <span className="font-semibold">{flor.categoria}</span>
-              
             </div>
             <Tag value={flor.estatus} severity={getSeverity(flor)}></Tag>
           </div>
-          
-          <div className="flex flex-column align-items-center gap-3 py-2">
-          <div className="text-3xl font-bold">{flor.nombre}
-            <img className="w-12 shadow-2 border-round" src={`https://media.admagazine.com/photos/61eb22cb9b19d943aa117b30/master/w_1600%2Cc_limit/Girasol.jpg`} alt="girasol" />
-            </div>
+
+          <div className="flex flex-column align-items-center gap-3 py-5">
+            <img className="shadow-2 border-round" src={`${flor.imagen}`} alt={`${flor.nombre}`} style={{ width: '200px', height: '200px' }} />
+            <div className="text-2xl font-bold">{flor.nombre}</div>
             <span className="text-2xl font-bold">${flor.precio}</span>
             {/* <Rating value={flor.rating} readOnly cancel={false}></Rating> */}
           </div>
-          
-          <div className="flex align-items-center justify-content-between  ">
-            <Button  icon="pi pi-heart" rounded severity="help" aria-label="Favorite" className="" />
-            <Button label="Detalles" icon="pi pi-search" className=" font-light ml-2" onClick={() => onClick('displayBasic')} />
-            <Button label="Agregar" icon="pi pi-shopping-cart" className="font-light ml-2 "disabled={flor.estatus === 'agotado'}></Button>
-           
-            <Dialog header={<h2><i className="pi pi-search"></i> Rosa</h2>}  draggable={false}  className="custom-dialog-header" visible={displayBasic} style={{ width: '25vw' }} footer={renderFooter('displayBasic')} onHide={() => onHide('displayBasic')}>
-            <img className="w-8 shadow-3 border-round " src={`https://media.admagazine.com/photos/61eb22cb9b19d943aa117b30/master/w_1600%2Cc_limit/Girasol.jpg`} width={'20%'} alt="girasol" />
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation </p>    
-            </Dialog>
-            
+
+          <div className="flex align-items-center justify-content-between">
+            <Button icon="pi pi-heart" rounded severity="help" aria-label="Favorite" className="" />
+            <Button label="Detalles" icon="pi pi-search" className=" font-light ml-2" onClick={() => dialogoFlor(flor)} />
+            <Button label="Agregar" icon="pi pi-shopping-cart" className="font-light ml-2 " disabled={flor.estatus === 'agotado'}></Button>
           </div>
+
         </div>
-       
       </div>
-      
     );
   };
 
@@ -182,23 +144,34 @@ const CatalogoFlores = () => {
   //--> Barra para cambiar modo de vista
   const header = () => {
     return (
-      <div className="flex justify-content-end">
-
+      <div className="flex justify-content-between">
         <span className="p-input-icon-left pr-2">
-        <i className="pi pi-search" />
-        <InputText placeholder="Buscar" value={searchTerm} onChange={onSearch} />
-        <Button icon="pi pi-search" className="p-button-warning"/>
-                                
+          <i className="pi pi-search" />
+          <Button icon="pi pi-search" className="p-button-warning" />
+          <InputText placeholder="Buscar" />
         </span>
-        <DataViewLayoutOptions value={filteredFlores} layout={layout} onChange={(e) => setLayout(e.value)} />
-        
-   
+
+        <DataViewLayoutOptions layout={layout} onChange={(e) => setLayout(e.value)} />
       </div>
-   
-       
     );
   };
 
+  //----------------| Funciones para dialogo |----------------
+  const dialogoFlor = (flor) => {
+    setMostrarDialog(true)
+    setDetallesFlor(flor)
+  }
+
+  const cerrarDialogo = () => {
+    setMostrarDialog(false)
+    setDetallesFlor({})
+  }
+
+  const botonesDialogo = (
+    <><Button label="Cerrar" icon="pi pi-times" onClick={cerrarDialogo} className="p-button-text" /></>
+  )
+
+  //----------------| Valor que regresara |----------------
   return (
     <Layout
       title="Flores"
@@ -207,12 +180,28 @@ const CatalogoFlores = () => {
       <div className="grid">
         <div className="col-12">
           <div className="card">
-            <h3>Catalogo de flores</h3>
-            <DataView value={filteredFlores} itemTemplate={itemTemplate} layout={layout} header={header()} />
+
+            <h5>Flores</h5>
+            <DataView value={flores} itemTemplate={itemTemplate} layout={layout} header={header()} />
+
+            <Dialog
+              header={`Detalles de ${detallesFlor.nombre}`}
+              visible={mostrarDialog} onHide={cerrarDialogo}
+              footer={botonesDialogo} style={{ width: '50vw' }}
+            >
+              <div className="mx-auto">
+                <img src={detallesFlor.imagen} style={{ width: '200px', height: '200px' }} />
+                <p className="m-0">Nombre:{detallesFlor.nombre}</p>
+                <p className="m-0">Precio:{detallesFlor.precio}</p>
+                <p className="m-0">Categoria:{detallesFlor.categoria}</p>
+                <p className="m-0">Estatus: {detallesFlor.estatus}</p>
+                <p className="m-0">Descripcion: {detallesFlor.descripcion}</p>
+              </div>
+            </Dialog>
+
           </div>
         </div>
       </div>
-      <Paginator first={0} rows={12} totalRecords={120} rowsPerPageOptions={[10, 20, 30]} /*onPageChange={onPageChange}*/ />
     </Layout>
   )
 }
